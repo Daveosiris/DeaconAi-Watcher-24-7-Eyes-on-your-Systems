76d3224400838eac3f29a5c28588d33bf1a0ba4c
theblackdeacon <apple@deaconsarl.com>
2023-06-30 11:55:10 +0300

    initial commit

---
+++ pages/footer.php
@@ -0,0 +1,12 @@
+    <br /></div>
+      <footer>
+        <div class="mt-auto d-flex justify-content-center">
+          <div class="col-lg-8">
+            <br />
+            <p>Protected by <strong><a href="https://www.deaconai.io" target="_blank">WATCHER by DeaconAi</a></strong></p>
+          </div>
+        </div>
+      </footer>
+
+  </body>
+</html>
